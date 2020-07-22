f7453829408721a5bb107a0feaa95d4afd678bcf
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-07-22 12:26:18 +0100

    created functions.ts file

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 function add(n1, n2) {
     return n1 + n2;
 }
+// use void if a function returns nothing, or let TS infer this itself
 function printResult(num) {
     console.log('Result: ' + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 console.log(printResult(add(5, 12)));
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+console.log(combineValues(8, 8));
+// let someValue = undefined
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
