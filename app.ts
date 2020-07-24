9bb998e1c7f5fb23a60a5b91f2973eab610a6f7b
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-07-24 14:45:25 +0100

    reviewing classes in TS

--- app.ts
+++ app.ts
@@ -1,3 +1,70 @@
-const userName = 'Shawn';
+class Department {
+  // private readonly id: string;
+  // private name: string;
+  private employees: string[] = [];
 
-console.log(userName)
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`)
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee)
+  }
+
+  printEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+class ITDepartment extends Department {
+  admins: string[]
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT');
+    this.admins = admins;
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting');
+  }
+
+  addReport(report: string) {
+    this.reports.push(report)
+  }
+
+  printReports() {
+    console.log(this.reports)
+  }
+}
+
+const sales = new Department('123', 'Sales');
+
+sales.addEmployee('Max');
+sales.addEmployee('Shawn');
+sales.describe();
+sales.printEmployeeInfo();
+
+
+const help = new ITDepartment('321', ['Shinji'])
+
+help.addEmployee('Shinji');
+help.addEmployee('Marie');
+console.log(help);
+
+
+const accounting = new AccountingDepartment('d2', [])
+
+console.log(accounting);
+accounting.addReport('Something went wrong...');
+accounting.printReports();
+
+// const accountingCopy = { name: 'Accounting Copy', describe: accounting.describe };
+
+// accountingCopy.describe()
